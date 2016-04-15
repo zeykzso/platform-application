04e39ba9f0beaa627f5ee570cc169435f83d0d6f
Boldizsár Zeyk Zsombor <zboldizsar@pitechplus.com>
2016-04-15 12:32:57 +0300

    OOT-1253 Email activity

---
+++ src/OroAcademy/Bundle/IssueTrackerBundle/Migrations/Schema/v1_1/OroAcademyIssueTrackerBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace OroAcademy\Bundle\IssueTrackerBundle\Migrations\Schema\v1_1;
+
+use Doctrine\DBAL\Schema\Schema;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
+use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+
+class OroAcademyIssueTrackerBundle implements Migration, ActivityExtensionAwareInterface
+{
+    /** @var ActivityExtension */
+    protected $activityExtension;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setActivityExtension(ActivityExtension $activityExtension)
+    {
+        $this->activityExtension = $activityExtension;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        $this->activityExtension->addActivityAssociation($schema, 'oro_email', 'oroacademy_issues');
+    }
+}
